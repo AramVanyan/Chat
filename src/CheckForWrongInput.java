28232bcbd12323bf772e7ae825779c59b3daf001
Aram Vanyan <aram.vanyan.a@gmail.com>
2019-12-24 17:20:50 +0400

    checks for inappropriate users input

---
+++ src/CheckForWrongInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CheckForWrongInput {
+    public static int checkForWrongInput(){
+        int input;
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            try {
+                input = Integer.parseInt(scanner.nextLine());
+                return input;
+            }catch (NumberFormatException e){
+                System.out.print("Please enter number: ");
+            }
+        }
+    }
+}
